8171c2514d8c9f7484f0ff331e6a1de5bfd95d38
Vik <vikbit.2025@gmail.com>
2026-09-15 21:00:14 -0700

    Refactor snowball throwing logic into a function

--- game_logic.js
+++ game_logic.js
@@ -9,16 +9,27 @@ setInterval(() => {
     throw_snowball = true;
 }, 1000);
 
-if (throw_snowball) {
-    // Creating another snowball image
-    let snowball = document.getElementById("snowball");
-    let snowball_clone = snowball.cloneNode(true);
-
-    // Giving it a unique ID
-    let snowball_cloneID = `snowball${snowball_number}`;
-    snowball_clone.id = snowball_cloneID;
-
-    // Actually putting it in the HTML
-    document.body.appendChild(snowball_clone);
+function throw_snowball_function() {
+    while (true) {
+        if (throw_snowball) {
+            // Creating another snowball image
+            let snowball = document.getElementById("snowball");
+            let snowball_clone = snowball.cloneNode(true);
+        
+            // Giving it a unique ID
+            let snowball_cloneID = `snowball${snowball_number}`;
+            snowball_clone.id = snowball_cloneID;
+        
+            // Actually putting it in the HTML
+            document.body.appendChild(snowball_clone);
+        
+            // Adding 1 to snowball_number-
+            snowball_number++;
+        } else {
+            continue;
+        }
+    }
 
 };
+
+document.addEventListener("click", throw_snowball_function);
